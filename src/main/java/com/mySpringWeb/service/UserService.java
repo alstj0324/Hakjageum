a336930b5d8b326f649049c5584c626111ed15be
alstj0324 <gomdung79@gmail.com>
2023-10-29 02:34:14 +0900

    add Signin Verification
    회원가입 유효성 검증기능 구현
    signin.jsp내부 자바스크립트 파일
    resources>js>signin.js로 따로구분

--- src/main/java/com/mySpringWeb/service/UserService.java
+++ src/main/java/com/mySpringWeb/service/UserService.java
@@ -8,8 +8,9 @@ public interface UserService {
 	void updateUser(UserVO vo);
 	void roleupdateUser(UserVO vo);
 	void deleteUser(UserVO vo);
-	String logincheckUser(UserVO vo);
 	String checkUser(UserVO vo);
+	String logincheckUser(UserVO vo);
+	String logincheckNickname(UserVO vo);
 	UserVO getUser(UserVO vo);
 	UserVO getUserLogin(UserVO vo);
 	List<UserVO> getUserList();
