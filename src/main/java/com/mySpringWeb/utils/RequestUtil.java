0cd5352533a5e5ec1382226605f17ce79eeab330
Kimchaegyu <kcg4344157@naver.com>
2023-10-28 16:22:42 +0900

    외부 API 요청 Util
    필요 주소만 기록되어 있음

---
+++ src/main/java/com/mySpringWeb/utils/RequestUtil.java
@@ -0,0 +1,10 @@
+package com.mySpringWeb.utils;
+
+public class RequestUtil {
+    private final String GET_BOOK_LIST = "https://openapi.naver.com/v1/search/book.json/";
+    private final String GET_BOOK_INFO = "https://openapi.naver.com/v1/search/book_adv.json/";
+    private final String GET_PLACE_KEYWORD = "https://dapi.kakao.com/v2/local/search/keyword.json/";
+    private final String GET_LOCATION = "http://ip-api.com/json/";
+    private final String GET_BLOG_LIST = "https://openapi.naver.com/v1/search/blog.json/";
+}
+
