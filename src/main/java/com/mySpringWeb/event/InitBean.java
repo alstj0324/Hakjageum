5ab6c4fe3c602256a716fad77be0d02f8865bef3
Kimchaegyu <kcg4344157@naver.com>
2023-10-30 10:20:44 +0900

    Event 등록

---
+++ src/main/java/com/mySpringWeb/event/InitBean.java
@@ -0,0 +1,36 @@
+package com.mySpringWeb.event;
+
+import com.mySpringWeb.domain.webhook.EmbedVO;
+import com.mySpringWeb.domain.webhook.HookLevel;
+import com.mySpringWeb.domain.webhook.HookVO;
+import com.mySpringWeb.service.DiscordWebhookServiceImpl;
+import com.mySpringWeb.utils.HookUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class InitBean {
+    @Autowired
+    private Environment environment;
+
+    @PostConstruct
+    public void init() {
+        try {
+            DiscordWebhookServiceImpl discordWebhookService = new DiscordWebhookServiceImpl();
+            List<EmbedVO> embedList = new ArrayList<>();
+            HookUtil hookUtil = new HookUtil();
+            EmbedVO embedVO = hookUtil.Info_Embed("서버 실행", "✅ 정상적으로 실행되었습니다.");
+            embedList.add(embedVO);
+            HookVO hookVO = hookUtil.create_Hook (HookLevel.INFO, embedList);
+            discordWebhookService.sendWebhook(hookVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
